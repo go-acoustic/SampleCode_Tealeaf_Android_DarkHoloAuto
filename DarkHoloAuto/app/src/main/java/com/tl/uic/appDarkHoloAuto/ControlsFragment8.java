3d90c01287e28cdbd47d98fc75bfe74c1ca5edc3
KJSweeneyMetova <kimberly.sweeney@metova.com>
2022-11-14 17:04:54 -0500

    updated app to use new functionality in the SDK

--- DarkHoloAuto/app/src/main/java/com/tl/uic/appDarkHoloAuto/ControlsFragment8.java
+++ DarkHoloAuto/app/src/main/java/com/tl/uic/appDarkHoloAuto/ControlsFragment8.java
@@ -9,6 +9,8 @@
  ********************************************************************************************/
 package com.tl.uic.appDarkHoloAuto;
 
+import static com.tl.uic.util.TLFConnectionUtil.openConnection;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,8 +22,7 @@ import androidx.fragment.app.Fragment;
 
 import com.tl.uic.Tealeaf;
 import com.tl.uic.model.Connection;
-import com.tl.uic.util.TLFOKHttpConnection;
-import com.tl.uic.util.TLFURLConnection;
+import com.tl.uic.util.TLFConnectionUtil;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -70,24 +71,22 @@ public class ControlsFragment8 extends Fragment {
                     URL url = new URL(siteUrl);
 
                     //example of automatically opening an http connection and logging the Connection properties
-                    HttpURLConnection httpClient = TLFURLConnection.openConnection(url);
-                    //example of automatically opening an http connection and logging the Connection properties
-//                    Object[] clientObject = openConnection(getContext(), siteUrl, ConnectionType.URL, ResponseType.DEFAULT, null);
+                    Object[] clientObject = openConnection(getContext(), siteUrl, TLFConnectionUtil.ConnectionType.URL, TLFConnectionUtil.ResponseType.DEFAULT, null);
 
                     //example of updating the connection properties using the currently open http connection
                     //this is only needed if the app needs to modify/update specific data values
                     //otherwise, the above openConnection method is the only piece of code needed
                     //to re-iterate, the following lines of code are optional
-                    Connection connection = TLFURLConnection.getConnection();
+                    Connection connection = TLFConnectionUtil.getConnection();
                     //example of updating an existing property
                     connection.setLoadTime(50);
                     //calculate approximate response time
                     connection.setResponseTime(new Date().getTime() - connection.getInitTime());
                     //will need to manually make another call  after updating properties
-                    TLFURLConnection.setConnection(connection);
+                    TLFConnectionUtil.setConnection(connection);
 
                     //example of how the response data can be extracted
-//                    HttpURLConnection httpClient = (HttpURLConnection) clientObject[0];
+                    HttpURLConnection httpClient = (HttpURLConnection) clientObject[0];
                     BufferedReader in = new BufferedReader(new InputStreamReader(httpClient.getInputStream()));
                     String inputLine;
                     while ((inputLine = in.readLine()) != null) {
@@ -110,23 +109,15 @@ public class ControlsFragment8 extends Fragment {
                 try {
                     String urlOKHttp = "https://jsonplaceholder.typicode.com/todos/1";
                     try {
-                        //example of automatically opening an OKHttp connection, logging the Connection properties, and returning the Response as a string
-                        Object[] test1 = TLFOKHttpConnection.runOKHttpConnection(urlOKHttp, "STRING");
-                        String stringResponse = (String) test1[0];
-                        Log.i("TESTING", "String Response = " + stringResponse);
-                        //example of automatically opening an OKHttp connection, logging the Connection properties, and returning the Response in Bytes
-                        Object[] test2 = TLFOKHttpConnection.runOKHttpConnection(urlOKHttp, "BYTES");
-                        ByteString byteResponse = (ByteString) test2[0];
-                        Log.i("TESTING", "Bytes Response = " + byteResponse);
 
                         //example of automatically opening an OKHttp connection, logging the Connection properties, and returning the Response as a string
-//                        Object[] test1 = openConnection(getContext(), urlOKHttp, ConnectionType.OKHTTP, ResponseType.STRING, null);
-//                        String stringResponse = (String) test1[0];
-//                        Log.i("TESTING", "OKHttp String Response = " + stringResponse);
-//                        //example of automatically opening an OKHttp connection, logging the Connection properties, and returning the Response in Bytes
-//                        Object[] test2 = openConnection(getContext(), urlOKHttp, ConnectionType.OKHTTP, ResponseType.BYTES, null);
-//                        ByteString byteResponse = (ByteString) test2[0];
-//                        Log.i("TESTING", "OKHttp Bytes Response = " + byteResponse);
+                        Object[] test1 = openConnection(getContext(), urlOKHttp, TLFConnectionUtil.ConnectionType.OKHTTP, TLFConnectionUtil.ResponseType.STRING, null);
+                        String stringResponse = (String) test1[0];
+                        Log.i("TESTING", "OKHttp String Response = " + stringResponse);
+                        //example of automatically opening an OKHttp connection, logging the Connection properties, and returning the Response in Bytes
+                        Object[] test2 = openConnection(getContext(), urlOKHttp, TLFConnectionUtil.ConnectionType.OKHTTP, TLFConnectionUtil.ResponseType.BYTES, null);
+                        ByteString byteResponse = (ByteString) test2[0];
+                        Log.i("TESTING", "OKHttp Bytes Response = " + byteResponse);
 
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -141,11 +132,11 @@ public class ControlsFragment8 extends Fragment {
         button = v.findViewById(R.id.btnVolleyConnection);
         button.setOnClickListener(view -> {
             String urlVolley = "https://jsonplaceholder.typicode.com/posts/";
-//            try {
-//                openConnection(getContext(), urlVolley, ConnectionType.VOLLEY, ResponseType.DEFAULT, response -> Log.i("TESTING", "Volley Response = " + response));
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
+            try {
+                openConnection(getContext(), urlVolley, TLFConnectionUtil.ConnectionType.VOLLEY, TLFConnectionUtil.ResponseType.DEFAULT, response -> Log.i("TESTING", "Volley Response = " + response));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         });
 
         button = v.findViewById(R.id.button5);
